6a4056df6e9ad05bd4ddd2aff152e010b00b7661
Zeeshan Haider <zhadier39@gmail.com>
2022-03-31 22:49:40 +0500

    fix errors

--- list.rb
+++ list.rb
@@ -1,7 +1,6 @@
-require_relative "enumerable"
+require_relative 'enumerable'
 
-class MyList 
-
+class MyList
   include MyEnumerable
 
   def initialize(*list)
@@ -12,18 +11,17 @@ class MyList
     i = 0
     while i < @list.length
       yield @list[i]
-      i+=1
+      i += 1
     end
   end
-
 end
 
 list = MyList.new(1, 2, 3, 4)
 
 puts list.all? { |e| e < 5 }
-puts list.all? {|e| e > 5}
+puts list.all? { |e| e > 5 }
 
-puts list.any? {|e| e == 2}
-puts list.any? {|e| e == 5}
+puts list.any? { |e| e == 2 }
+puts list.any? { |e| e == 5 }
 
-puts list.filter {|e| e.even?}
+puts list.filter(&:even?)
